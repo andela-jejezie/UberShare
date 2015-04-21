1ca2856d472e1579a48bfe5809c41d131c736424
Johnson Ejezie <johnsonejezie@Johnsons-MacBook-Pro.local>
2015-04-21 12:03:40 +0100

    Initial Commit

---
+++ UberShare/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UberShare
+//
+//  Created by Johnson Ejezie on 4/21/15.
+//  Copyright (c) 2015 Johnson Ejezie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ UberShare/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UberShare
+//
+//  Created by Johnson Ejezie on 4/21/15.
+//  Copyright (c) 2015 Johnson Ejezie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
